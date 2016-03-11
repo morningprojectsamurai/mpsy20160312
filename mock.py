0ae430b5c62155265cc8947a7c07c48b3247119c
Junya Kaneko <jyuneko@gmail.com>
2016-03-11 23:44:14 +0900

    first commit

---
+++ mock.py
@@ -0,0 +1,32 @@
+# This file is part of "Lecture materials at Morning Project Samurai Yokohama 2016/03/12"
+#
+# "Lecture materials at Morning Project Samurai Yokohama 2016/03/12"
+# is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Lecture materials at Morning Project Samurai Yokohama 2016/03/12"
+# is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# (c) Junya Kaneko <jyuneko@hotmail.com>
+
+
+__author__ = 'Junya Kaneko <jyuneko@hotmail.com>'
+
+
+class Element:
+    def __init__(self, w):
+        self._w = None
+
+    def f(self, x):
+        pass
+
+    def get_value(self, x):
+        pass
